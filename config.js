22352394658b1af1c1d39912dd0809e275517e1b
rizwanishaq <rizwanishaq@gmail.com>
2020-01-16 18:47:57 +0100

    user login and register completed

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
   MONGODB:
-    "mongodb+srv://rizwan123:rizwan123@devconnector-gxqbj.mongodb.net/merng?retryWrites=true&w=majority"
+    "mongodb+srv://rizwan123:rizwan123@devconnector-gxqbj.mongodb.net/merng?retryWrites=true&w=majority",
+
+  SECRET_KEY: "some very secret key"
 };
